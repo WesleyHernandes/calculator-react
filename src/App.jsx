25257359b7f55838081e9f67c4eebb21c03038bd
WesleyHernandes <wess.hernandes@gmail.com>
2022-08-18 20:22:02 -0300

    Calculator Logic

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,60 @@ export default class App extends Component {
 
   setOperation = (operation) => {
     console.log(operation)
+    if(this.state.current === 0){
+      this.setState({ operation, current:1, clearDisplay:true })
+    }else{
+      const equals = operation === '='
+      const currentOperation = this.state.operation
+
+      const values = [...this.state.values]
+
+      switch(currentOperation){
+        case '+': 
+          values[0] = (values[0] + values[1]) 
+        break
+        case '-': 
+          values[0] = values[0] - values[1] 
+          break
+        case '*': 
+          values[0] = values[0] * values[1] 
+          break
+        case '/': 
+          values[0] = values[0] / values[1] 
+          break
+      }
+
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   setDigit = (value) => {
     if(value === '.' && this.state.displayValue.includes('.')){
       return
     }
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? "" : this.state.displayValue
+    const displayValue = currentValue + value
+    this.setState({ displayValue, clearDisplay:false })
+
+    if(value !== '.'){
+      const i = this.state.current
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[i] = newValue
+      this.setState({ values })
+    }
+
+    console.log(this.state.values)
   }
 
   render(){
@@ -36,8 +84,8 @@ export default class App extends Component {
           <Display value={this.state.displayValue} />
           <Panel 
             clearMemory={() => this.clearMemory()} 
-            setOperation={() => this.setOperation()} 
-            setDigit={() => this.setDigit()} 
+            setOperation={(v) => this.setOperation(v)} 
+            setDigit={(v) => this.setDigit(v)} 
           />
         </div>
       </div>
--- src/panel/Panel.jsx
+++ src/panel/Panel.jsx
@@ -7,25 +7,25 @@ const Panel = props => {
     return(
         <div className="Panel">
             <div className="Buttons-Content">
-                <Button label="CE" size="large" click={props.clearMemory} />
-                <Button label={7} />
-                <Button label={8} />
-                <Button label={9} />
-                <Button label={4} />
-                <Button label={5} />
-                <Button label={6} />
-                <Button label={1} />
-                <Button label={2} />
-                <Button label={3} />
-                <Button label="." />
-                <Button label={0} size="medium" />
+                <Button label="CE" size="large" click={_ => props.clearMemory()} />
+                <Button label={7} click={_ => props.setDigit(7)} />
+                <Button label={8} click={_ => props.setDigit(8)} />
+                <Button label={9} click={_ => props.setDigit(9)} />
+                <Button label={4} click={_ => props.setDigit(4)} />
+                <Button label={5} click={_ => props.setDigit(5)} />
+                <Button label={6} click={_ => props.setDigit(6)} />
+                <Button label={1} click={_ => props.setDigit(1)} />
+                <Button label={2} click={_ => props.setDigit(2)} />
+                <Button label={3} click={_ => props.setDigit(3)} />
+                <Button label="." click={_ => props.setDigit('.')} />
+                <Button label={0} size="medium" click={_ => props.setDigit(0)} />
             </div>
             <div className="Operators-Content">
-                <Button label="/" theme="gradient" />
-                <Button label="*" theme="gradient" />
-                <Button label="-" theme="gradient" />
-                <Button label="+" theme="gradient" />
-                <Button label="=" theme="gradient" />
+                <Button label="/" theme="gradient" click={_ => props.setOperation('/')} />
+                <Button label="*" theme="gradient" click={_ => props.setOperation('*')} />
+                <Button label="-" theme="gradient" click={_ => props.setOperation('-')} />
+                <Button label="+" theme="gradient" click={_ => props.setOperation('+')} />
+                <Button label="=" theme="gradient" click={_ => props.setOperation('=')} />
             </div>
         </div>
     )
